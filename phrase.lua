b6d7d46e6b59f23a3fbea6d1f1730a2e7a3562d0
Jon Allen <ylixir@gmail.com>
2015-06-25 09:08:24 -0500

    finished? yeah right

--- phrase.lua
+++ phrase.lua
@@ -10,7 +10,8 @@ as published by Sam Hocevar. See the COPYING file for more details.
 ]]--
 
 --[[ these are the functions that we will be using ]]--
-local main, menu, load, generate, show
+local main, menu, load_lists, generate, display_options
+local change_list, settings
 
 --[[ these are the 'classes' we will use ]]--
 local object = require 'object'
@@ -18,7 +19,7 @@ local protodice = require 'dice'
 local protolist = require 'list'
 
 --[[ the defaults are stored in these variables ]]--
-local phrase_length, list, random_letter
+local phrase_length, use_list, random_letter
 
 --[[ the word lists and supporting data are stored here ]]--
 local lists, letter_list
@@ -37,18 +38,41 @@ for i=1,32 do letter_list[i] = string.char(0x20 + i) end
 --everything starts here
 function main()
   phrase_length = 7
-  list = lists['diceware8k.list']
+  use_list = 'diceware8k.list'
   random_letter = true --insert a random letter into the phrase?
 
-  --just load the lists so we don't have to deal with it
-  load()
+  load_lists()
   
   while true do
+    display_options()
     menu
     ({
-      ['Update the word lists']=function() dofile('update.lua') load() end,
-      ['Generate a pass phrase']=function() show(generate()) end,
+      ['Generate a pass phrase']=generate,
+      ['Update the word lists from the interwebs']=function()
+        dofile('update.lua')
+        load_lists()
+      end,
+      ['Change settings']=settings,
       ['Quit']=os.exit
+    })
+  end
+end
+
+function settings()
+  local done = false
+  while not done do
+    display_options()
+    menu
+    ({
+      ['Use a different word list']=change_list,
+      ['Toggle random character insertion']=function()
+        random_letter = not random_letter
+      end,
+      ['Change the length of the phrase']=function()
+        io.write('How many words do you want in your phrase? ')
+        phrase_length = math.floor(io.read('*n'))
+      end,
+      ['Back']=function() done=true end
     })
   end
 end
@@ -65,48 +89,58 @@ function menu(choices)
   indexes[io.read('*n')]()
 end
 
+function display_options()
+  io.write('\nUsing '..use_list)
+  io.write(' to generate a phrase of length '..phrase_length)
+  if random_letter then
+    io.write(' which includes a random character.\n\n')
+  else
+    io.write(' which does not include a random character.\n\n')
+  end
+end
+
+function change_list()
+  menu_table = {}
+  for k,v in pairs(lists )do
+    menu_table[k] = function() use_list=k end
+  end
+  menu(menu_table)
+end
+
 function generate()
-  --first read the diceware list into memory
+  print('This may take a moment if your computer needs more entropy.')
+  print('You should be using your computer to make this go faster.')
+  print('Go surf the internet or something.')
+  local phrase =  ''
+  local the_list = lists[use_list]
+  for i = 1,phrase_length do
+    phrase = phrase..the_list.words[the_list.dice:roll()]..' '
+  end
+  phrase = phrase:sub(1,-2)
   
-  local phrase = {}
-  for i = 1,phrase_length do phrase[i] = list.words[list.dice:roll()] end
-  
-  if true == random_letter then
-    local dice, letter, word, place
-    dice = protodice:new()
-    dice.throws = 1
+  if random_letter then
+    local dice, letter, place
     
-    dice.sides = #letter_list
+    dice = protodice:new()
+    dice.throws=1
+    dice.sides=#letter_list
+    
     letter = letter_list[dice:roll()]
     
-    dice.sides = phrase_length
-    word = dice:roll()
-    
-    dice.sides  = #phrase[word]+1
+    dice.sides = #phrase+1
     place = dice:roll()
-    
-    if 1 == place then
-      phrase[word] = letter..phrase[word]
-    elseif #phrase[word]+1 == place then
-      phrase[word] = phrase[word]..letter
-    else
-      phrase[word] = string.sub(phrase[word],1,place-1)
-                     ..letter ..string.sub(phrase[word],place,-1)
-    end
+    phrase = string.sub(phrase,1,place-1)
+             ..letter
+             ..string.sub(phrase,place,-1)
   end
+  
+  io.write('\n'..phrase..'\n\n')
+  
   return phrase
 end
 
-function show(phrase)
-  for i,v in ipairs(phrase) do
-    if i > 1 then io.write(' ') end
-    io.write(v)
-  end
-  io.write('\n')
-end
-
---(re)load all the lists from disk
-function load()
+--(re)load all lists from disk
+function load_lists()
   for k,v in pairs(lists) do
     v:load(k) --we conveniently stored each word list under it's filename
   end
